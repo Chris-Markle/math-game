410a9055644aaf0c4ff2be9294cda024190c8d39
Chris Markle <christofermarkle@gmail.com>
2020-06-16 14:59:11 -0700

    1) Ones now carried to next column. 2) No 0's allowed in hunds column

--- js/app.js
+++ js/app.js
@@ -10,33 +10,69 @@ const ansRowOnes = document.querySelector('.ar-o');
 const ansRowTens = document.querySelector('.ar-t');
 const ansRowHunds = document.querySelector('.ar-h');
 
-let additionAnswer;
+let onesAnswer;
+let tensAnswer;
+let hundsAnswer;
+
 
 const randNum = (val) => {
   const randVal = Math.floor(Math.random() * 10);
-  val.innerHTML = `${randVal}`;
+  number = val.innerHTML = `${randVal}`;
 }
 
 const assignValues = () => {
   randNum(topRowOnes);
   randNum(topRowTens);
-  randNum(topRowHunds);
+  randNum(topRowHunds)
   randNum(botRowOnes);
   randNum(botRowTens);
   randNum(botRowHunds);
+  let checkTopHunds = document.querySelector('.tr-h').innerHTML;
+  let checkBotHunds = document.querySelector('.br-h').innerHTML;
+  if(checkTopHunds === '0') {
+    randNum(topRowHunds);
+    // let newHunds = document.querySelector('.tr-h').innerHTML;
+    // console.log(`Hundreds place changed to ${newHunds}`)
+  }
+
+  if(checkBotHunds === '0') {
+    randNum(botRowHunds);
+  }
 }
 
 assignValues();
 
 const checkAdditionAnswer = () => {
-  let onesAnswer = topRowOnes + botRowOnes;
-  let tensAnswer = topRowTens + botRowTens;
+
+  let topOnes = Number(document.querySelector('.tr-o').innerHTML);
+  let botOnes = Number(document.querySelector('.br-o').innerHTML);
+  let topTens = Number(document.querySelector('.tr-t').innerHTML);
+  let botTens = Number(document.querySelector('.br-t').innerHTML);
+  let topHunds = Number(document.querySelector('.tr-h').innerHTML);
+  let botHunds = Number(document.querySelector('.br-h').innerHTML);
+
+  onesAnswer = topOnes + botOnes;
+  tensAnswer = topTens + botTens;
+  hundsAnswer = topHunds + botHunds;
 
   if(onesAnswer > 9) {
+    let onesString = onesAnswer.toString();
+    let dblDigitNum = onesString.split("");
+    onesAnswer = Number(dblDigitNum[1]);
     tensAnswer += 1;
   } else {
     return onesAnswer;
   }
+  
+  if(tensAnswer > 9) {
+    let tensString = tensAnswer.toString();
+    let dblDigitNum = tensString.split("");
+    tensAnswer = Number(dblDigitNum[1]);
+    hundsAnswer += 1;
+  } else {   
+    return tensAnswer;
+  }
+  console.log(hundsAnswer, tensAnswer, onesAnswer);
 }
 
 checkAdditionAnswer();
